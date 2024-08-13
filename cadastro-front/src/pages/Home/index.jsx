5c6885f7c19b5c6072b2d0d73f6dc49f8d79b753
thiagoAlves0 <thiago.alvessb1@gmail.com>
2024-08-13 11:30:41 -0300

    correction

--- cadastro-front/src/pages/Home/index.jsx
+++ cadastro-front/src/pages/Home/index.jsx
@@ -1,30 +1,41 @@
 import './style.css'
 import Trash from '../../assets/16qg.svg'
 import api from '../../services/api'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 
 function Home() {
-const [users, setUsers]  = useState([])
+  const [users, setUsers]  = useState([])
+
+  const inputName = useRef()
+  const inputAge = useRef()
+  const inputEmail = useRef()
 
   async function getUsers(){
-  const usersFromApi =  await api.get('/usuarios')
+    const usersFromApi = await api.get('/usuarios')
+    setUsers(usersFromApi.data)
+  }
 
-  setUsers(usersFromApi.data)
+  async function createUsers(){
+    await api.post('/usuarios', {
+      name: inputName.current.value,
+      age: inputAge.current.value,
+      email: inputEmail.current.value, // Correção aqui
+    })
+    getUsers(); // Atualizar a lista de usuários após a criação
   }
 
   useEffect(() => {
     getUsers()
   }, [])
 
-
   return (
     <div className="container">
       <form>
         <h1>Cadastro de usuários</h1>
-        <input placeholder="Nome" name="nome" type="text" />
-        <input placeholder="Idade" name="idade" type="number" />
-        <input placeholder="E-mail" name="email" type="email" />
-        <button type="button">Cadastrar</button>
+        <input placeholder="Nome" name="nome" type="text" ref={inputName}/>
+        <input placeholder="Idade" name="idade" type="number" ref={inputAge}/>
+        <input placeholder="E-mail" name="email" type="email" ref={inputEmail}/>
+        <button type="button" onClick={createUsers}>Cadastrar</button>
       </form>
 
       {users.map((user) => (
@@ -35,7 +46,7 @@ const [users, setUsers]  = useState([])
             <p>E-mail: <span>{user.email}</span></p>
           </div>
           <button>
-            <img src={Trash} />
+            <img src={Trash} alt="Deletar usuário"/>
           </button>
         </div>
       ))}
